bfbce8cd79a933bf29847bbbd51d7033360ba468
oluseyi-ged <oluseyi.ged@gmail.com>
2021-06-09 20:44:46 +0100

    Initializing messaging app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
 
 function App() {
+  const [input, setInput] = useState("")
+  console.log(input)
+  const [messages, setMessages] = useState([])
+  console.log(messages)
+
+  const sendMessage = (event) => {
+    // logic involving sending message
+    setMessages([...messages, input])
+    setInput("")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quick Reach</h1>
+
+      <input value={input} onChange={(event) => setInput(event.target.value)} />
+      <button onClick={sendMessage}>✉ send</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
